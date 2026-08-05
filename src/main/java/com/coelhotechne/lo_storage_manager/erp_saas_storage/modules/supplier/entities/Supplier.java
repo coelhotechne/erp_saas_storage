eff3cc2c31ee8c91b13c1a1fc690ef7699333a4a
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-05 14:49:35 -0300

    updated: Supplier v1: project: erp_saas_storage date: 05082026

--- src/main/java/com/coelhotechne/lo_storage_manager/erp_saas_storage/modules/supplier/entities/Supplier.java
+++ src/main/java/com/coelhotechne/lo_storage_manager/erp_saas_storage/modules/supplier/entities/Supplier.java
@@ -1,8 +1,7 @@
-package com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.supplier;
+package com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.supplier.entities;
 
-import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.address.AddressBr;
+import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.address.entities.AddressBr;
 import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.principals.entities.Entidade;
-import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.product.Product;
 import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.product.ProductSupplier;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
@@ -11,7 +10,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CNPJ;
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 import tools.jackson.databind.PropertyNamingStrategies;
@@ -44,15 +42,6 @@ public class Supplier extends Entidade {
     @OneToMany(mappedBy = "supplier",fetch = FetchType.LAZY,cascade = CascadeType.ALL,orphanRemoval = true)
     private List<ProductSupplier>productSuppliers = new ArrayList<>();
 
-    @CPF
-    @Column(length = 11,unique = true)
-    private String cpf;
-
-    // Muitos registros podem ter um produto, economia de chamadas no banco
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "supplier_product")
-    private Product product;
-
     //Data de contratação
     @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
@@ -68,6 +57,7 @@ public class Supplier extends Entidade {
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
     private LocalDateTime contractTerminationDate;
     @Column(name = "address_br",nullable = false)
+    @Embedded
     private AddressBr addressBr;
 
 }
