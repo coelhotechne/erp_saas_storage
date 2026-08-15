daa992ffeeebebba0ed32025f18cf6b90e5a9ca3
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-15 11:11:26 -0300

    updated: AddressBr.java v1: project: erp_saas_storage date: 15082026

--- src/main/java/com/coelhotechne/lo_storage_manager/erp_saas_storage/modules/address/entities/Address.java
+++ src/main/java/com/coelhotechne/lo_storage_manager/erp_saas_storage/modules/address/entities/Address.java
@@ -1,11 +1,39 @@
 package com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.address.entities;
 
-import jakarta.persistence.Embeddable;
+import com.coelhotechne.lo_storage_manager.erp_saas_storage.modules.address.enums.Country;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Embeddable
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_address")
+@AllArgsConstructor
 public class Address {
-    private String street;
-    private String city;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Country country;
+    @Column(nullable = false)
+    private String postalCode;
+    @Column(nullable = false)
     private String state;
-    private String zipCode;
+    @Column(nullable = false)
+    private String city;
+
+    private String district;
+
+    @Column(nullable = false)
+    private String street;
+
+    private String number;
+
+    private String complement;
+
 }
